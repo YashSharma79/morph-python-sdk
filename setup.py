8ebc761447a22708e8b6c20c828e0e51ec018d36
Andrei Betlen <abetlen@gmail.com>
2024-12-10 17:29:27 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,43 @@
+from setuptools import find_packages, setup
+from setuptools.command.install import install
+
+
+class PostInstallCommand(install):
+    def run(self):
+        install.run(self)
+
+
+setup(
+    name="morphcloud",
+    version="0.1.10",
+    use_scm_version=True,
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "requests",
+        "tqdm",
+        "argparse",
+        "shutil",
+        "psutil",
+        "streamlit",
+        "anthropic",
+        "openai",
+        "pillow",
+        "httpx",
+        "fire",
+        "playwright",
+        "bs4",
+        "click",
+        "pytest",
+        "pydantic",
+        "httpx",
+    ],
+    entry_points={
+        "console_scripts": [
+            "morphcloud=morphcloud:main",
+        ],
+    },
+    cmdclass={
+        "install": PostInstallCommand,
+    },
+)
